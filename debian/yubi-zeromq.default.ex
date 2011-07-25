5be510d19ce7bcae60c95864b6e0e894e8dee6a9
Antoine Sirinelli <antoine@monte-stello.com>
2011-07-25 22:20:21 +0100

    Debian files to be updated.

---
+++ debian/yubi-zeromq.default.ex
@@ -0,0 +1,10 @@
+# Defaults for yubi-zeromq initscript
+# sourced by /etc/init.d/yubi-zeromq
+# installed at /etc/default/yubi-zeromq by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
